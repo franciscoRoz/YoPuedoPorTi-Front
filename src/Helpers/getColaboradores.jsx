2004b618f4c017e34e23f3f9c9274371d166286c
Francisco Rozas <francisco.rozas@outlook.com>
2023-11-13 16:41:18 -0300

    push

--- src/Helpers/getColaboradores.jsx
+++ src/Helpers/getColaboradores.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getColaboradores = async (Servicio) => {
   let URL = `https://yopuedoporti.dataecom.cl/Colaborador/ListaTrabajadores/${Servicio}`;
  
-
+  console.log(URL);
  
   try {
   
